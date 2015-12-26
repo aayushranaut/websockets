04ea07a2f83272a508bc85e8ed2748ab99f78e77
Aayush Ranaut <aayush.ranaut@gmail.com>
2015-12-26 14:37:05 +0530

    Added SocketIO

--- server/main.js
+++ server/main.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.get('/', function(req, res) {
   res.send('Hello World');
   console.log("Something just connected to express");
+});
+
+io.on('connection', function(socket) {
+  console.log('Something connected to socket.io');
+  socket.emit('messages', ["Hello", "How are you?", "WOW!"]);
 });
 
 server.listen(8080);
