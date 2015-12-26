c0d43e71ce5082675a0c6d91361e823d16ee8573
Aayush Ranaut <aayush.ranaut@gmail.com>
2015-12-26 23:09:58 +0530

    generated form for liking

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 var userId = localStorage.getItem("userId") || randomId();
 localStorage.setItem("userId", userId);
 console.info("User: " + userId);
+var messageCache;
 
 function randomId() {
   return Math.floor(Math.random() * 1e11);
@@ -9,16 +10,19 @@ function randomId() {
 var socket = io.connect('http://localhost:8080', {'forceNew': true});
 socket.on("messages", function(data) {
   console.info(data);
+  messageCache = data;
 
-  var html = data.map(function(data) {
+  var html = data.map(function(data, index) {
     return (`
-      <div class='name'>
-        ${data.userName}
-      </div>
-      <a href=${data.content.link} class='message' target='_blank'>
-        ${data.content.text}
-      </a>
-      <input type=submit class="likes-count" value="${data.likedBy.length} likes" />
+      <form class="message" onsubmit="return likeMessage(messageCache[${index}]);">
+        <div class='name'>
+          ${data.userName}
+        </div>
+        <a href=${data.content.link} class='message' target='_blank'>
+          ${data.content.text}
+        </a>
+        <input type=submit class="likes-count" value="${data.likedBy.length} likes" />
+      </form>
     `);
   }).join(" ");
 
